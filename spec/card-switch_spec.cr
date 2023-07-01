fff2e40bf45dd488a8afbf65c9fdebf7282bf8e6
Michael Wagner <mdwranger@gmail.com>
2023-07-01 18:06:17 -0400

    init

---
+++ spec/card-switch_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Card::Switch do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/card-switch.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Card::Switch`
+module Card::Switch
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
